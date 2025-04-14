36700240df405ea6c0b2450d4badbd0f84c4e3f3
Tomas Villar <t.villar@alumno.um.edu.ar>
2025-04-14 01:54:04 -0300

    Agrego en la funcion is_palindrome la implementacion de caracteres y agrego una funcion que permitirá ejecuatr el programa principal para ingresar entradas por consola para verificar si son palindromes, se puede precionar control + c para terminar programa

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,2 +1,23 @@
 def is_palindrome(word:str) -> bool:
     word= word.lower().replace(" ","").replace(":","").replace(",","").replace(".","").replace("?","")
+    j = -1
+    resultado = True
+    for i in word:
+         j += 1
+         n = (j*(-1)-1)
+         if i != (word[n]):
+            resultado = False
+         n = 0
+    return resultado
+def ejecucion_programa():
+    while True:
+        my_word= input("Ingrese una palabra o frase: ")
+        is_palindrome(my_word)
+        resultado1 = is_palindrome(my_word)
+        if resultado1 == True:
+            print(f"{my_word} es palindorme")
+        else:
+            print(f"{my_word} no es palindrome")
+
+if __name__ == "__main__":
+    ejecucion_programa()
